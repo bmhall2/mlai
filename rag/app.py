3d572cc6e553af9d398aec0bf8bd4790202a52d7
Ben Hall <ben@allocate.co>
2023-12-20 14:21:26 -0600

    split loader and runner scripts

--- rag/app.py
+++ rag/app.py
@@ -1,72 +1,64 @@
-import bs4
+import argparse
 from langchain import hub
-from langchain.chat_models import ChatOpenAI
-from langchain.document_loaders import TextLoader, DirectoryLoader
 from langchain.embeddings import OpenAIEmbeddings
+from langchain.chat_models import ChatOpenAI
 from langchain.schema import StrOutputParser
-from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.vectorstores import Chroma
 from langchain_core.runnables import RunnablePassthrough
-import os
 
-from langchain.document_loaders import WebBaseLoader
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--query", required=True, type=str)
+    parser.add_argument("--fileId", required=True, type=int)
 
-loader = DirectoryLoader(
-    '/mnt/c/Learning/mlai/PdfTextTest/text/',
-    glob="**/*.txt",
-    show_progress=True,
-    loader_cls=TextLoader
-)
+    args = parser.parse_args()
+    query = args.query
+    fileId = args.fileId
 
-docs = loader.load()
+    open_api_key = ''
 
-# print(len(docs))
-# print(len(docs[0].page_content))
-# print(docs[0].page_content[:500])
+    client = Chroma(
+        collection_name="rag",
+        persist_directory="./chroma",
+        embedding_function = OpenAIEmbeddings(
+            api_key=open_api_key
+        )
+    )
 
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000, chunk_overlap=200, add_start_index=True
-)
-all_splits = text_splitter.split_documents(docs)
+    retriever = client.as_retriever(
+        search_type="similarity",
+        search_kwargs={
+            "k": 6,
+            "filter" : {'fileId':fileId}
+        })
 
-# print(len(all_splits))
-# print(len(all_splits[0].page_content))
-# print(all_splits[10].metadata)
+    retrieved_docs = retriever.get_relevant_documents(query)
 
-os.environ["OPENAI_API_KEY"] = ''
-vectorstore = Chroma.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+    # print(retrieved_docs[0].metadata)
 
-# Done with Indexing
+    if len(retrieved_docs) > 0:
 
-retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
+        llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, api_key=open_api_key)
 
-retrieved_docs = retriever.get_relevant_documents(
-    "What is the name of the Partnership?"
-)
+        prompt = hub.pull("rlm/rag-prompt")
 
-# print(len(retrieved_docs))
+        # print(
+        #     prompt.invoke(
+        #         {"context": "filler context", "question": "filler question"}
+        #     ).to_string()
+        # )
 
-# print(retrieved_docs[0].page_content)
+        def format_docs(docs):
+            return "\n\n".join(doc.page_content for doc in docs)
 
-llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
+        rag_chain = (
+            {"context": retriever | format_docs, "question": RunnablePassthrough()}
+            | prompt
+            | llm
+            | StrOutputParser()
+        )
 
-prompt = hub.pull("rlm/rag-prompt")
-
-# print(
-#     prompt.invoke(
-#         {"context": "filler context", "question": "filler question"}
-#     ).to_string()
-# )
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-rag_chain = (
-    {"context": retriever | format_docs, "question": RunnablePassthrough()}
-    | prompt
-    | llm
-    | StrOutputParser()
-)
-
-for chunk in rag_chain.stream("What is the name of the Partnership?"):
-    print(chunk, end="", flush=True)
+        for chunk in rag_chain.stream(query):
+            print(chunk, end="", flush=True)
+    else:
+        print("No local results")
